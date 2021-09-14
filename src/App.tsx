2f1a7f8ab492a45a690e4d4bf3d019367ab37aa8
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-09-14 10:40:11 +0500

    created and cleaned some folder&files

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Global mentoring react</h1>
-
+      <p>Hello world!</p>
     </div>
   );
-}
+} 
 
 export default App;
--- src/dist/App.js
+++ src/dist/App.js
@@ -3,6 +3,7 @@ exports.__esModule = true;
 var react_1 = require("react");
 function App() {
     return (react_1["default"].createElement("div", { className: "App" },
-        react_1["default"].createElement("h1", null, "Global mentoring react")));
+        react_1["default"].createElement("h1", null, "Global mentoring react"),
+        react_1["default"].createElement("p", null, "Hello world!")));
 }
 exports["default"] = App;
